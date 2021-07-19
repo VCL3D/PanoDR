20a88b40ad5f38d2ffc28e763005fa8f7f596de2
Vasilis <gkitsasv@iti.gr>
2021-07-19 13:55:43 +0300

    Updated inference code

--- src/metrics/pytorch_ssim/__init__.py
+++ src/metrics/pytorch_ssim/__init__.py
@@ -9,7 +9,7 @@ def gaussian(window_size, sigma):
     return gauss/gauss.sum()
 
 def create_window(window_size, channel):
-    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _1D_window = PanoDR(window_size, 1.5).unsqueeze(1)
     _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
     window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
     return window
---
+++ src/train/__init__.py
@@ -0,0 +1,2 @@
+from .mask2poly_mult import *
+from .Structured_3D_refined import *
